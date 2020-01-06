2a6e986d7589b93c9edcc7bd98baaf41962a4c4f
naveen-pete <naveen.shenoy.blr@gmail.com>
2020-01-06 17:40:51 +0530

    commit code at the end of day 1

---
+++ apps/day-1/ts-examples/app.js
@@ -0,0 +1,4 @@
+var my_name = 'Hari';
+var salary = 10000;
+console.log('Name:', my_name);
+console.log('Salary:', salary);
---
+++ apps/day-1/ts-examples/app.ts
@@ -0,0 +1,5 @@
+var my_name: string = 'Hari';
+var salary: number = 10000;
+
+console.log('Name:', my_name);
+console.log('Salary:', salary);
